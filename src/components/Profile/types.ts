4320e11d0af9975ef767c61696e3b4a577bcf1cc
ceejell <challeanka@gmail.com>
2025-07-24 22:07:51 +0200

    Added components files, no code tho

---
+++ src/components/Profile/types.ts
@@ -0,0 +1,5 @@
+export type ProfileProps = {
+    username: string;
+    achievementlist: string; // change to achievements part, done as seperate component
+    friendlist: string; // change to friendlist part, done as seperate component
+}
