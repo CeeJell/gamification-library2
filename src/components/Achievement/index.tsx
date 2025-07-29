b7ba4f82ca49d713763bdc9349650ffdf798d57b
ceejell <challeanka@gmail.com>
2025-07-30 00:51:26 +0200

    testing children on Achievement

--- src/components/Achievement/index.tsx
+++ src/components/Achievement/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Props } from './types';
 import ProgressBar from '../ProgressBar';
 
-const Achievement: React.FC<Props> = ({ title, description, picture, percentage }) => {
+const Achievement: React.FC<Props> = ({ title, description, picture, percentage }, {children}) => {
     return (
         <div style={{border: "3px solid ", content: "", clear: "both", display: "table", borderColor: "red", borderRadius: "8px"}}>
             <img src={picture} style={{float: "left", margin: "3px", border: "1px solid", borderColor: "black", padding: "1px"}}/>
@@ -12,7 +12,7 @@ const Achievement: React.FC<Props> = ({ title, description, picture, percentage 
             <h2>{description}</h2>
             <ProgressBar percentage={percentage}></ProgressBar>
             </div>
-
+            {children}
         </div>
     );
 };
